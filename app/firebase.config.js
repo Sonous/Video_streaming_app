f4021bf21bbff503b182b872231bb2d62688681b
Lam Huy <163141119+Sonous@users.noreply.github.com>
2024-10-24 21:19:35 +0700

    fix env variable

--- app/firebase.config.js
+++ app/firebase.config.js
@@ -2,32 +2,30 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/storage';
-import 'firebase/firestore';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import 'firebase/compat/firestore';
+import Config from 'react-native-config';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
+    apiKey: Config.API_KEY,
+    authDomain: Config.AUTH_DOMAIN,
+    projectId: Config.PROJECT_ID,
+    storageBucket: Config.STORAGE_BUCKET,
+    messagingSenderId: Config.MESSAGING_SENDER_ID,
+    appId: Config.APP_ID,
+    measurementId: Config.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
-// Initialize Firebase Authentication and get a reference to the service
-const auth = firebase.auth();
 const storage = firebase.storage();
+const auth = firebase.auth();
 const db = firebase.firestore();
 
-export default firebase;
-export { auth, storage, db };
+export { firebase, storage, auth, db };
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import MainLayout from '../layouts/MainLayout';
+// import { storage } from '../../firebase.config';
+
+// const storageRef = storage.ref();
+// storageRef
+//     .child('videos/test.mp4')
+//     .getDownloadURL()
+//     .then((res) => console.log(res));
 
 export default function Home() {
     return (
